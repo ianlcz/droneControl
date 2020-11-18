1d60edcae2229cee4bb7fd3b74da4148539b6736
Yann LE COZ <yann.lecoz@ynov.com>
2020-11-18 10:08:32 +0100

    Changing the color of the background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const AppStyled = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+  body {
+    position: relative;
+    margin: 0;
+    padding: 0;
+    min-height: 100%;
+    background: linear-gradient(111.34deg, #0b3268 0%, #114da1 100%) no-repeat fixed;
+  }
+`;
 
 const App = () => (
-  <div style={{ textAlign: "center" }}>
+  <React.Fragment>
+    <AppStyled />
     <h1>Ydays - droneControl</h1>
     <h2>You are in droneControl app !</h2>
-  </div>
+  </React.Fragment>
 );
 
 export default App;
