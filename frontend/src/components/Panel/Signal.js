110fc1f8124e546ab43019053f6c7c3ce3a3de4c
Yann LE COZ <yann.lecoz@ynov.com>
2020-12-04 18:26:39 +0100

    Update Signal component
    It not function in realtime.

--- frontend/src/components/Panel/Signal.js
+++ frontend/src/components/Panel/Signal.js
@@ -1,15 +1,34 @@
 import React from "react";
-import { RiSignalWifiOffFill } from "react-icons/ri";
+import {
+  RiSignalWifi1Fill,
+  RiSignalWifi2Fill,
+  RiSignalWifi3Fill,
+  RiSignalWifiFill,
+  RiSignalWifiOffFill,
+} from "react-icons/ri";
 import styled from "styled-components";
 
-const SignalIcon = styled(RiSignalWifiOffFill)`
+const SignalStyled = styled.li`
   font-size: 2em;
 `;
 
-const Signal = () => (
-  <li title="Signal Wi-Fi : 0dB">
-    <SignalIcon />
-  </li>
-);
+const Signal = () => {
+  let SNR = null;
+  return (
+    <SignalStyled title={`Signal Wi-Fi${SNR ? ` : ${SNR}dB` : ""}`}>
+      {SNR >= 41 ? (
+        <RiSignalWifiFill />
+      ) : SNR <= 40 && SNR >= 25 ? (
+        <RiSignalWifi3Fill />
+      ) : SNR <= 24 && SNR >= 16 ? (
+        <RiSignalWifi2Fill />
+      ) : SNR <= 15 && SNR >= 10 ? (
+        <RiSignalWifi1Fill />
+      ) : (
+        <RiSignalWifiOffFill />
+      )}
+    </SignalStyled>
+  );
+};
 
 export default Signal;
